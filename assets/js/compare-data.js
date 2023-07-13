97acb96f28daaadc11de12d5d669950349b92fe4
Humair <romeosmarty143@gmail.com>
2023-07-13 18:49:23 +0500

    About Us Change In About

---
+++ assets/js/compare-data.js
@@ -0,0 +1,93 @@
+var product = [{
+    "id": 0,
+    "title": "",
+    "price": "",
+    "description": "",
+    "brand": "",
+    "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/1665px-No-Image-Placeholder.svg.png"
+}, {
+    "id": 1,
+    "title": "PlayStation 2",
+    "price": 20000,
+    "description": "Featuring a sleek and compact design, the PlayStation 2 offered backward compatibility with its predecessor, allowing players to enjoy a vast library of PlayStation games. It also introduced a built-in DVD player, making it a versatile entertainment device. With its powerful Emotion Engine processor and Graphics Synthesizer, the PS2 delivered stunning visuals and immersive gaming experiences",
+    "brand": "Sony",
+    "image": "https://compareprice.pk/wp-content/uploads/2020/03/playstation-2-scph-90004-console-black-price.jpg"
+}, {
+    "id": 2,
+    "title": "PlayStation 3",
+    "price": 40000,
+    "description": "The console featured a powerful multicore Cell processor, capable of delivering impressive graphics and computational performance. It also included a built-in Blu-ray Disc drive, which allowed users to play high-definition movies and enjoy immersive gaming experiences with enhanced graphics and sound.",
+    "brand": "Sony",
+    "image": "https://i5.walmartimages.com/asr/e07d46d9-808f-4ea2-97f1-83e30273fe5d_1.10057129bfa2a699060a0d0e37b41db2.jpeg"
+}, {
+    "id": 3,
+    "title": "PlayStation 4",
+    "price": 55000,
+    "description": "The PS4 features a sleek and stylish design, with a black matte finish and angular edges. It is equipped with powerful hardware, including an x86-64 AMD Jaguar eight-core processor and a custom AMD Radeon GPU, enabling impressive graphics and smooth gameplay.",
+    "brand": "Sony",
+    "image": "https://www.bhphotovideo.com/images/images500x500/sony_3000767_ps4_500gb_four_games_1416936912_1099070.jpg"
+}, {
+    "id": 4,
+    "title": "PlayStation 5",
+    "price": 85000,
+    "description": "The PS5 boasts a sleek and futuristic design, with a striking white and black color scheme. Its powerful hardware includes a custom-designed CPU and GPU, enabling advanced graphics and smooth gameplay at up to 4K resolution and 120 frames per second. Additionally, the console supports ray tracing, which enhances lighting and visual effects for a more realistic and detailed gaming experience.",
+    "brand": "Sony",
+    "image": "https://compareprice.pk/wp-content/uploads/2020/06/sony-playStation-5-1tb-console-white-price.jpg"
+}, {
+    "id": 5,
+    "title": "Xbox Series X",
+    "price": 80000,
+    "description": "The Xbox Series X is a high-powered gaming console developed by Microsoft. It offers a cutting-edge gaming experience with impressive graphics, fast loading times, and smooth gameplay. The console is equipped with a custom-designed AMD Zen 2 processor and RDNA 2 graphics architecture, enabling it to deliver 4K gaming at up to 120 frames per second. It supports ray tracing, which enhances visual realism, and offers quick resume functionality, allowing players to seamlessly switch between multiple games.",
+    "brand": "Xbox Microsoft",
+    "image": "https://90a1c75758623581b3f8-5c119c3de181c9857fcb2784776b17ef.ssl.cf2.rackcdn.com/662646_536110_03_package_comping.jpg"
+}, {
+    "id": 6,
+    "title": "X Box 360",
+    "price": 28000,
+    "description": "One of the defining features of the Xbox 360 was its online service, Xbox Live. Through Xbox Live, players could connect with friends, compete in multiplayer games, and access a variety of online content such as game demos, downloadable content, and streaming services. Xbox Live fostered a vibrant online community and provided an avenue for socializing and engaging with other gamers.",
+    "brand": "Xbox Microsoft",
+    "image": "https://www.itinternational.pk/wp-content/uploads/2020/12/Microsoft-Xbox-360-Ultra-Slim-250-GB-Price-in-Pakistan.jpg"
+}, {
+    "id": 7,
+    "title": "Xbox One S",
+    "price": 35000,
+    "description": "The Xbox One S, you can play a vast library of Xbox One games, including both physical discs and digital downloads. It supports high-definition gaming with improved graphics and faster performance. The console also supports HDR (High Dynamic Range) technology, which enhances the visual quality of compatible games, making them more vibrant and lifelike.",
+    "brand": "Xbox Microsoft",
+    "image": "https://5.imimg.com/data5/FP/MC/PK/SELLER-14158318/microsoft-xbox-one-s-all-digital-edition-gaming-console-500x500.jpg"
+}, {
+    "id": 8,
+    "title": "Xbox One X",
+    "price": 60000,
+    "description": "The Xbox One X supports backward compatibility, allowing you to play select Xbox 360 and original Xbox games. Additionally, it offers access to a wide range of entertainment apps, including streaming services like Netflix and Hulu. With its enhanced performance and immersive features, the Xbox One X provides a premium gaming experience for players.",
+    "brand": "Xbox Microsoft",
+    "image": "https://m.media-amazon.com/images/I/41qqpkrlKmL.jpg"
+}, {
+    "id": 9,
+    "title": "Nitendo",
+    "price": 8000,
+    "description": "Nintendo Switch: A hybrid video game console that can be used as a handheld device or connected to a TV. It offers a wide range of games and features detachable controllers called Joy-Cons. Nintendo Switch Lite: A compact version of the Nintendo Switch designed solely for handheld gaming. It features an integrated control pad and is lighter and more portable than the original Switch.",
+    "brand": "Nitendo",
+    "image": "https://www.tradeinn.com/f/13734/137343676/nintendo-switch.jpg"
+}, {
+    "id": 10,
+    "title": "Genesis",
+    "price": 5000,
+    "description": "The game features a wide range of customizable characters with unique abilities, allowing players to tailor their gameplay experience to their preferences. Whether exploring vast landscapes, participating in cooperative multiplayer missions, or competing in exhilarating player-versus-player battles, Genesis offers a diverse and engaging gaming experience that keeps players hooked for hours on end.",
+    "brand": "Genesis",
+    "image": "https://i.ebayimg.com/images/g/APAAAOSwlvtdfjUw/s-l500.jpg"
+
+}, {
+    "id": 11,
+    "title": "Sega",
+    "price": 3000,
+    "description": "Sega is a renowned video game company that has created a wide range of products loved by gamers around the world. ",
+    "brand": "Sega",
+    "image": "https://www.buygame2.com/media-cache/bg2_product/500x500/2022-06/20220606164006-hogk3hz1nep6.webp"
+},{
+    "id": 12,
+    "title": "Nitendo 3Ds",
+    "price": 12000,
+    "description": "The console features dual screens, with the top screen being a 3.53-inch autostereoscopic display capable of showing 3D graphics, and the bottom screen being a 3.02-inch touch-sensitive display. It also includes a built-in microphone, motion sensors, and a front-facing camera for augmented reality experiences.",
+    "brand": "Nitendo",
+    "image": "https://i.ebayimg.com/images/g/JTEAAOSwcFJjccB2/s-l500.jpg"
+}]
